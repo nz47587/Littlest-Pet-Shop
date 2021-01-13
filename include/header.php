eef2b4d8092734652708c2ae68fda90989ae7174
fk48404 <75526863+fk48404@users.noreply.github.com>
2021-01-13 12:02:38 +0100

    Create header.php

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<div class="header">
+    <div class="nav">
+        <img id="divH1" src="images/lpslogo2.png" alt="">
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./dogs.php">Dogs</a></li>
+                <li><a href="./cats.php">Cats</a></li>
+                <li><a href="./bunnies.php">Bunnies</a></li>
+                <li><a href="./educateme.php">Educate me</a></li>
+				<li><a href="./about.php">About us</a></li>
+				<li><a href="./adoptMe.php">Adopt me</a></li>
+                <li><a href="./loginregister.php">Login/Register</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
