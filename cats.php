f5beada4aef9fce73266c86f05ed229fea528a9e
Nereida Zhitopotoku <73653968+nz47587@users.noreply.github.com>
2021-01-17 16:19:04 +0100

    Create cats.php

---
+++ cats.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Macet</title>
+        <link href="css/cats.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+<!--HEADER-->
+        <?php include 'include/header.php'; ?>
+
+                                            <!--Cat page-->
+        <div class="macja-parent">
+            <div class="cat-child">
+                <h1>------- Perla -------</h1>
+                    <div class="cat">
+                        <img  id="cat-img"  src="images/Perla.png"/>
+                        <img  id="cat-img"  src="images/Perla1.png"/>
+                        <img  id="cat-img"  src="images/Perla2.png"/>
+                    </div>                      
+                    <h3>Rreth Perles</h3>
+                    <p>Perla eshte mace persiane dhe ka 9 muaj. Eshte mace shume e lezetshme dhe e adhuron aromen e parfumit dhe te shampos se flokeve.
+                        Perla eshte nje nga macet me te ndrojtura ne institutin tone, por kjo nuk do te thote se nuk do te ja u rregulloje diten tuaj
+                        dhe te ju sjelle plot gjalleri ne shtepine tuaj. Perla poashtu i do shume femijet dhe eshte perfekte per ambiete ku jane te 
+                        pranishem edhe femijet.</p>
+        
+                        <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+                        
+                    </div>
+            <div class="cat-child">
+                <h1>------- Luna -------</h1>
+                    <div class="cat">
+                        <img  id="cat-img" src="images/Luna.png"/>
+                        <img  id="cat-img" src="images/Luna1.png"/>
+                        <img  id="cat-img" src="images/Luna2.png"/>
+                    </div>   
+                   <h3>Rreth Lunes</h3>
+                   <p>Luna eshte mace e llojit "Russian blue" dhe ka 1 vit e gjysem. Eshte mace me klas dhe eshte nje nga macet me te sjellshme 
+                    ne institut. Luna adhuron te luaj "dress up" me vajzat e vogla te cilat vijne qe ta vizitojne, dhe te gjithe femijet te cilet
+                    e takojna Lunen e adhurojne ate. Ajo do te ishte perfekte per nje ambient si shtepia juaj.</p>
+
+
+                    <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+
+            </div>
+            <div class="cat-child">
+                <h1>------- Ginger -------</h1>
+                    <div class="cat">
+                        <img  id="cat-img" src="images/Ginger.png"/>
+                        <img  id="cat-img" src="images/Ginger1.png"/>
+                        <img  id="cat-img" src="images/Ginger2.png"/>
+                    </div>
+                    <h3>Rreth Gingerit</h3>
+                    <p>Gingeri eshte mace e llojit "Munchkin" dhe eshte macja me e vogel qe kemi, me vetem 6 muaj. Gingeri eshte shume lozonjare 
+                        dhe shume e hareshme, i adhuron gjerat te cilat shkelqejne dhe supen me peshk. Gingeri ka tendence qe te jete pak qapkene 
+                        por prap se prap me femijet eshte shume e sjellshme. Gingeri do te ishte shume e gezuar po te ishte pjese e familjes suaj.</p>
+                        <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+            </div>
+            <div class="cat-child">
+                <h1>------- Topolake -------</h1>
+                    <div class="cat">
+                        <img  id="cat-img" src="images/Topolake.png"/>
+                        <img  id="cat-img" src="images/Topolake1.png"/>
+                        <img  id="cat-img" src="images/Topolake2.png"/>
+                    </div> 
+                <h3>Rreth Topolakes</h3>
+                <p>Topolakja eshte mace Siberiane dhe ka 9 muaj. Gjerat e saj te preferuara jane shetitjet ne park, koha e larjes ne pishine
+                    dhe lojerat e saje. Topolaken zakonisht e gjeni duke u munduar te dal nga dhoma e saje per ndonje shetitje spontane, e qe 
+                    fale shikimit te saje aq te embel, ndonje femije ia hape deren dhe Topolakja keshtu e arrine qellimin e saje.
+                    Gjithsesi, ajo gjithmone kthehet ne kohe per dreke.</p>
+                    <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+            </div>
+        </div>
+    </body>
+    <footer>
+        <?php include 'include/footer.php'; ?>
+    </footer>
+
+</html>
