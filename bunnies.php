cb72216179b4e51b4f896fb2d9f9f673e9e50abb
Nereida Zhitopotoku <73653968+nz47587@users.noreply.github.com>
2021-01-17 16:20:06 +0100

    Create bunnies.php

---
+++ bunnies.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lepujt</title>
+        <link href="css/bunnies.css" rel="stylesheet" type="text/css"/>
+    </head>
+
+    <body>
+<!--HEADER-->
+        <?php include 'include/header.php'; ?>
+
+                                            <!--bunnies page-->
+        <div class="bunny-parent">
+
+            <div class="bunny-child">
+                <h1>------- Susu -------</h1>
+                    <div class="bunny">
+                        <img  id="bunny-img"  src="images/Susu.png"/>
+                        <img  id="bunny-img"  src="images/Susu1.png"/>
+                        <img  id="bunny-img"  src="images/Susu2.png"/>
+                    </div>                      
+                                                                 <h3>Rreth Susu-se</h3>
+                    <p>Susu eshte lepur i llojit "Miniature Lop" dhe eshte 5 muajshe. Susu-se i pelqene shume fjongot te cilat ia vendosim
+                    ne koke dhe cdo dite deshiron qe ta nderroj llojin e fjongos dhe ngjyren e saje. Eshte lepuroshe shume inteligjente
+                    dhe e lezetshme. Ne pranine e femijeve eshte e matur dhe e sjellshme dhe asnjehere nuk ka ndodhe qe te kemi probleme 
+                    me te. Susu eshte e pershtatshme per lloje te ndryshme te ambienteve, asaj i nevojitet vetem nje familje e cila e do 
+                    shume.</p>
+                        <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+                </div>
+
+                <div class="bunny-child">
+                <h1>------- Stormi -------</h1>
+                <div class="bunny">
+                        <img  id="bunny-img" src="images/Stormi.png"/>
+                        <img  id="bunny-img" src="images/Stormi1.png"/>
+                        <img  id="bunny-img" src="images/Stormi2.png"/>
+                        
+                    </div>   
+                                                                <h3>Rreth Stormit</h3>
+                    <p>Stormi eshte lloj i lepuve "Plush Lop" dhe i ka 4 muaj. Stormi e adhuron natyren dhe karrotat e saje, andaj gjithnje eshte
+                    diku neper kopsht duke luajtur ose duke ngrene. Stormi eshte me e vogla prej lepurosheve qe kemi dhe eshte shume e ndrojtur
+                    por prap se prap eshte kenaqesi te jeshe nen prezencen e saje. Kjo lepuroshe i adhuron femijet dhe eshte e pershtatshme per
+                    familje nga me te ndryshme.</p>
+                     <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+            </div>
+
+            <div class="bunny-child">
+                <h1>------- Nugget -------</h1>
+                <div class="bunny">
+                        <img  id="bunny-img" src="images/Nugget.png"/>
+                        <img  id="bunny-img" src="images/Nugget1.png"/>
+                        <img  id="bunny-img" src="images/Nugget2.png"/>
+                    </div>
+                                                                <h3>Rreth Nugget</h3>
+                    <p>Nugget eshte lepur i llojit "Havana" dhe eshte 5 muajsh. Nugget sillet sikur te ishte nje femij i rritur. Ai adhuron qantat
+                    e shpies, adhuron qe te dal ne shetitje me ato qanta sepse i duket vetja sikur te jete pjese e femijeve. Nugget eshte lepurosh 
+                    i bukur dhe i bute dhe shumicen e kohes eshte duke kercyer nga nje vend ne tjetrin me qanten e tij te shpines, keshtu duke sjelle
+                    buzeqshje ne te gjitha fetyrat tona. Nuget eshte me te vertete nje zgjedhje shume e mire per kafshe shtepiake.</p>
+             <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+            </div>
+
+            <div class="bunny-child">
+                <h1>------- Walnut -------</h1>
+                <div class="bunny">
+                        <img  id="bunny-img" src="images/Wallnut.png"/>
+                        <img  id="bunny-img" src="images/Wallnut1.png"/>
+                        <img  id="bunny-img" src="images/Wallnut2.png"/>                        
+                    </div> 
+                                                               <h3>Rreth Walnut</h3>
+            <p>Walnut eshte lepur i llojit "Golden Glavcot" dhe i ka 11 muaj. Walnut eshte lepurosh shume fotogjen dhe ka shume deshire te dal ne
+            fotografi. Walnut i do shume lulet, gjerat me ngjyra te ndezura dhe fushat e medha ku ka vend me se te mjaftueshem per te bredhur
+            ku te do ai. Duke qene se ka natyre te tille, edhe femijet e duan shume, dhe gjithnje na pyesin rreth tij. Sa i perket ushqimit
+            nuk eshte se zgjedh shume, por karrotat dhe lakrat jane te preferuarat e tij. Eshte zgjedhje shume e mire nese po kerkoni te 
+            adoptoni lepurosh, por mos harroni te mbani kameren gjithnje me vete.</p>
+            <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+            </div>
+
+        </div>
+    </body>
+    <footer>
+        <?php include 'include/footer.php'; ?>
+    </footer>
+
+</html>
