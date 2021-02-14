43d46028a23cba6ed64af55402acea081c5d154f
Nereida Zhitopotoku <73653968+nz47587@users.noreply.github.com>
2021-02-14 19:33:47 +0100

    Update dogs.php

--- dogs.php
+++ dogs.php
@@ -3,73 +3,41 @@
     <head>
         <title>Qenushet</title>
         <link href="css/dogs.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" type="text/css" href="mobile/dogs.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 
-<body background="lightblue">
+<body>
 <!--HEADER-->
     <?php include 'include/header.php'; ?>
-
+<?
+	$query = $pdo->query('SELECT * from dogs');
+	$dogs = $query->fetchAll();
+?>
        <!--dog page-->
         <div class="qeni-parent">
+        <?php foreach($dogs as $d1){ ?>
             <div class="qeni-child">
-                <h1>------- Kumbulla -------</h1>
+                <h1><? echo $d1['h1'];?></h1>
                     <div class="qeni">
-                        <img  id="qeni-img"  src="images\Kumbull.png"/>
-                        <img  id="qeni-img"  src="images\Kumbull1.png"/>
-                        <img  id="qeni-img"  src="images\Kumbull2.png"/>
+                        <img  id="qeni-img"  src="images\<? echo $d1['image1'];?>"/>
+                        <img  id="qeni-img"  src="images\<? echo $d1['image2'];?>"/>
+                        <img  id="qeni-img"  src="images\<? echo $d1['image3'];?>"/>
                     </div>                      
-                                                                 <h3>Rreth Kumbulles</h3>
-                    <p>Kumbulla eshte qen i llojit Samoyed dhe i ka 10 muaj. Eshte qen shume i hareshem dhe gjithnje gati per ndonje aventure 
-                    ne park. Kumbulla adhuron lulet dhe vemendja nga femijet eshte gjeja e preferuar e tij. Edhe pse adhuron ambientin e jashtem 
-                    Kumbulla ne te njejten kohe eshte me se i lumtur qe te qendroj brenda ne shtepi per sa kohe qe eshte nen pranine e familjes
-                    se tij.</p>
-                        <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
+                        <h3><? echo $d1['h3'];?></h3>
+                    <p><? echo $d1['text'];?></p>
+                        <button  id="button" type="submit"><a  id="linku-button" href="contact.html">Me Adopto!</a></button>
+
                 </div>
-            <div class="qeni-child">
-                <h1>------- Strudel -------</h1>
-                    <div class="qeni">
-                        <img  id="qeni-img" src="images\Strudel.png"/>
-                        <img  id="qeni-img" src="images\Strudel1.png"/>
-                        <img  id="qeni-img" src="images\Strudel2.png"/>
-                        
-                    </div>   
-                                                                <h3>Rreth Strudelit</h3>
-                    <p>Strudeli eshte qen i llojit Doberman dhe i ka 7 muaj. Eshte ende i vogel dhe mjaft i lezetshem. Edhe pse mund te duket 
-                    pak i frikshem ne fillim, ai eshte shume paqesore dhe shum i dashur. Femijet ne fillim frigohen, por ne momentin qe
-                    fillojne ta njohin Strudelin e duan shume. Strudeli adhuron lojen me frisbi, vrapimin e hershem ne park dhe nuk mund
-                    te jetoj pa ushqimin e tij. Ky lloj i qeneve eshte shume i paster dhe nuk paraqet problem ne mirmbajtjen e shtepise.</p>
-                     <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
-            </div>
-            <div class="qeni-child">
-                <h1>------- Lilo -------</h1>
-                    <div class="qeni">
-                        <img  id="qeni-img" src="images/Lilo.png"/>
-                        <img  id="qeni-img" src="images/Lilo1.png"/>
-                        <img  id="qeni-img" src="images/Lilo2.png"/>
-                    </div>
-                                                                <h3>Rreth Lilos</h3>
-                    <p>Lilo eshte qen husky dhe i ka 6 muaj. Lilo eshte qen i llastuar por shume i lezetshem dhe keshtu askush smund ti rezistoj 
-                    deshirave te tij. Ai i adhuron femijet dhe eshte gjithnje mese i gezuar ti ndihmoj ata. Lilo eshte qeni perfekt nese keni
-                    deshire qe shtepia juaj te jete gjithnje plot e perplot me te qeshura. </p>
-                    <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
-            </div>
-            <div class="qeni-child">
-                <h1>------- TenTen -------</h1>
-                    <div class="qeni">
-                        <img  id="qeni-img" src="images/TenTen.png"/>
-                        <img  id="qeni-img" src="images/TenTen1.png"/>
-                        <img  id="qeni-img" src="images/TenTen2.png"/>
-                    </div> 
-                                                               <h3>Rreth TenTenit</h3>
-                    <p>TenTen eshte qen i llojit "Woodle" dhe eshte 8 muajsh. TenTen e adhuron ujin dhe keshtu gjithnje eshte afer pishines 
-                    duke luajtur me qent e tjere apo edhe ndonje femi. Ushqim i preferuar i TenTenit eshte mishi i pules me salce kerpudhash.
-                    Eshte shume i sjellshem dhe eshte shume i pershtatshem per secilen familje e cila eshte e interesuar ta adoptoj.</p>
-                    <button  id="button" type="submit"><a  id="linku-button" href="adopto.php">Me Adopto!</a></button>
-            </div>
+                <?php } ?>
+           
         </div>
         <br></br>
     </body>
         <footer>
-            <?php include 'include/footer.php'; ?>
+                <div id="lastFooter">
+                <p>&copy; 2020 Littlest Pet Shop </p>
+                </div>
+            </div>
         </footer>
 </html>
